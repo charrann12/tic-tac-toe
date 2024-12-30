5747f9fcb1c73e65e181bbafbdda91a0929c49e4
Sai Charan Nethi <158495761+charrann12@users.noreply.github.com>
2024-12-30 23:31:12 +0530

    30-12-24 xox.js

--- xox.js
+++ xox.js
@@ -1,1 +1,16 @@
-alert("hello");
+console.log("Welcome to the XOX Game!");
+
+let music = new Audio("adds/music.mp3");
+let turn = new Audio("adds/turn.mp3");
+let gameOver = new Audio("adds/gameover.mp3");
+
+
+//function to change the turn 
+const changeTurn = ()=>{
+    return turn === "X" ?"0":"X";
+}
+
+//function to check for a win
+const checkWin =()=>{
+    
+}
